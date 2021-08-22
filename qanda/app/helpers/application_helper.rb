d34f2483fa07411482906adc3dcd67cdbf6f8a22
Uro3 <miyakawa.uro3@gmail.com>
2021-08-22 14:07:26 +0900

    show flash messages

--- qanda/app/helpers/application_helper.rb
+++ qanda/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def transform_alert_key(key)
+    case key
+    when 'alert'
+      'danger'
+    when 'notice'
+      'success'
+    else
+      'info'
+    end
+  end
 end
