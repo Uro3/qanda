9fbc5b422f6a5d162cb8826e823fe1c3eedb7189
Uro3 <miyakawa.uro3@gmail.com>
2021-08-22 14:19:46 +0900

    fix devise url error

--- qanda/config/routes.rb
+++ qanda/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#top'
-  devise_for :users, module: "users"
+  devise_for :users, module: 'users'
+  get '/users', to: redirect('/users/sign_up')
+  get '/users/password', to: redirect('/users/password/new')
   resources :questions
 end
