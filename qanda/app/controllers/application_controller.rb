df7686ee558f62a0b9d35eebc1547c9e5d67b6f7
Uro3 <miyakawa.uro3@gmail.com>
2021-08-22 13:42:49 +0900

    generate custom devise controllers

--- qanda/app/controllers/application_controller.rb
+++ qanda/app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
 
     def redirect_to_login_if_not_logged_in
       unless user_signed_in?
--- qanda/config/routes.rb
+++ qanda/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#top'
-  devise_for :users
+  devise_for :users, module: "users"
   resources :questions
 end
